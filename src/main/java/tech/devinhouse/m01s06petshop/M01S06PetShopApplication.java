defa2e5109f1837562993feab6c6162bfe29de2e
Joao P Manfredini <jpaulohm@gmail.com>
2022-11-02 12:02:15 -0300

    Criou projeto

---
+++ src/main/java/tech/devinhouse/m01s06petshop/M01S06PetShopApplication.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.m01s06petshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M01S06PetShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M01S06PetShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/devinhouse/m01s06petshop/M01S06PetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.m01s06petshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M01S06PetShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
